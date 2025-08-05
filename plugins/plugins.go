c2958ebf011d07e9575637eee5f065341d3be675
jxin.song <s7799653@qq.com>
2025-08-05 09:07:25 +0800

    修改获取不到节点的metrics信息，评分为0

--- plugins/plugins.go
+++ plugins/plugins.go
@@ -73,7 +73,7 @@ func (s *MetricsSchedulerPlugin) Score(ctx context.Context, state *framework.Cyc
 	nodeMetrics, err := metricsClient.MetricsV1beta1().NodeMetricses().Get(context.TODO(), nodeName, metav1.GetOptions{})
 	if err != nil {
 		log.Fatalf("Error getting node metrics %s: %s", nodeName, err.Error())
-		return -1, framework.NewStatus(framework.Error, "Error getting node metrics")
+		return 0, framework.NewStatus(framework.Success, "")
 	}
 	totalMemory := node.Status.Allocatable.Memory().Value()
 	usedMemory := nodeMetrics.Usage.Memory().Value()
